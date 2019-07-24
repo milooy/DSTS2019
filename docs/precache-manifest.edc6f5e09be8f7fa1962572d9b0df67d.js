b4cf6434ca4eff203abe06deeb4774b1f3f30f5f
Jay Jin <jayjinjay@gmail.com>
2019-07-25 06:38:50 +0900

    Revert "깃헙 페이지 SPA 적용을 위한 html 파일 및 라우팅 해킹 코드 추가"
    This reverts commit 76458e8d074071b935b0e75780e6cb5eba8e6b65.

---
+++ docs/precache-manifest.edc6f5e09be8f7fa1962572d9b0df67d.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "4492a223337112670372012df7fdc012",
+    "url": "/DSTS2019/img/송치오.4492a223.png"
+  },
+  {
+    "revision": "29bc94a32bc39eda6d8f566a34bd5183",
+    "url": "/DSTS2019/img/meta.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DSTS2019/robots.txt"
+  },
+  {
+    "revision": "ad4b57b8676a1fd9901d",
+    "url": "/DSTS2019/js/app.37a8648c.js"
+  },
+  {
+    "revision": "a6015316ef24e9df8112",
+    "url": "/DSTS2019/js/chunk-vendors.7892bc12.js"
+  },
+  {
+    "revision": "fe876cd53d327fb20f9dc21fd1f93470",
+    "url": "/DSTS2019/img/한성민.fe876cd5.png"
+  },
+  {
+    "revision": "8c7c2581fae32e962f7c94068f4ae479",
+    "url": "/DSTS2019/img/정산하.8c7c2581.png"
+  },
+  {
+    "revision": "5d3a47c12ddd179b47c369b15804963b",
+    "url": "/DSTS2019/img/변성윤.5d3a47c1.jpg"
+  },
+  {
+    "revision": "953d57e1a494cef971ede714e134e24b",
+    "url": "/DSTS2019/img/강민구.953d57e1.jpg"
+  },
+  {
+    "revision": "057d352d3e37dd58680171eaefd982d8",
+    "url": "/DSTS2019/img/최정윤.057d352d.png"
+  },
+  {
+    "revision": "4d8bc765bafa565fe76a08d59306953e",
+    "url": "/DSTS2019/img/심상훈.4d8bc765.png"
+  },
+  {
+    "revision": "24d173c871f6f199e19b5e89990ad53e",
+    "url": "/DSTS2019/img/김진영.24d173c8.jpg"
+  },
+  {
+    "revision": "57c753d34e93f02b7d53d99d0da72a4d",
+    "url": "/DSTS2019/img/송치오.57c753d3.jpg"
+  },
+  {
+    "revision": "b2807045230b2781b4f4af7d695b031f",
+    "url": "/DSTS2019/img/백영민.b2807045.png"
+  },
+  {
+    "revision": "5bf5a815bb7f6982251ff6b98aa23dab",
+    "url": "/DSTS2019/img/배준현.5bf5a815.jpg"
+  },
+  {
+    "revision": "e070bb84dd2e6790d60cff09ced13604",
+    "url": "/DSTS2019/img/이지혜.e070bb84.jpeg"
+  },
+  {
+    "revision": "f9555477677f0d4f699c60c96cb361d5",
+    "url": "/DSTS2019/index.html"
+  },
+  {
+    "revision": "02a1a39c19c39e4f88600c6bd236bf2f",
+    "url": "/DSTS2019/img/임송이.02a1a39c.png"
+  },
+  {
+    "revision": "021cf5d181964fcfdc5be80c4158ab8d",
+    "url": "/DSTS2019/img/양선영.021cf5d1.jpg"
+  },
+  {
+    "revision": "8fac00c6e02c8ab5d663f8cef00a8ee0",
+    "url": "/DSTS2019/img/김정화.8fac00c6.jpeg"
+  },
+  {
+    "revision": "e16fbdf7061b1c2c157e13e879a86187",
+    "url": "/DSTS2019/img/김진영.e16fbdf7.png"
+  },
+  {
+    "revision": "d59813d3a87d6f9012ba6500c739f57c",
+    "url": "/DSTS2019/img/백영민.d59813d3.png"
+  },
+  {
+    "revision": "459334bb509a3f284f689e4b800097c2",
+    "url": "/DSTS2019/img/배준현.459334bb.png"
+  },
+  {
+    "revision": "70f1728c0a805f80ec3c2d058505bc48",
+    "url": "/DSTS2019/img/uniform.70f1728c.png"
+  },
+  {
+    "revision": "cc1a765d2a555a4e6074",
+    "url": "/DSTS2019/js/about.b3a732dd.js"
+  },
+  {
+    "revision": "8bb9047b3d2a241d034170bc3b1946e8",
+    "url": "/DSTS2019/img/ticket.8bb9047b.png"
+  },
+  {
+    "revision": "0bed7d3cb1fe63081ee2f8dd939fbc1b",
+    "url": "/DSTS2019/img/김인범.0bed7d3c.png"
+  },
+  {
+    "revision": "40c524caf85b44c430332f2a56d4ef25",
+    "url": "/DSTS2019/img/임송이.40c524ca.png"
+  },
+  {
+    "revision": "e4eec0bae9d5488718df864d923327b5",
+    "url": "/DSTS2019/img/최정윤.e4eec0ba.png"
+  },
+  {
+    "revision": "f7fea9ff3a187a29a4b774b9209c4c9c",
+    "url": "/DSTS2019/img/김정화.f7fea9ff.png"
+  },
+  {
+    "revision": "1be58c916ea902f5ed89685e482f55a4",
+    "url": "/DSTS2019/img/강원양.1be58c91.png"
+  },
+  {
+    "revision": "d9fb06135435283f9ab5a6e33bd13e09",
+    "url": "/DSTS2019/img/김인범.d9fb0613.jpg"
+  },
+  {
+    "revision": "91dfa1b93b22b1ba9cf9dbbea087459c",
+    "url": "/DSTS2019/img/양선영.91dfa1b9.png"
+  },
+  {
+    "revision": "37199f4539869b54c8dee0014eef01b2",
+    "url": "/DSTS2019/img/uniform.37199f45.png"
+  },
+  {
+    "revision": "4f2d05194d378dc80aca6c5c69370da1",
+    "url": "/DSTS2019/img/정산하.4f2d0519.jpg"
+  },
+  {
+    "revision": "e6259d7bf3ceb71f59ddb71bdfe75e80",
+    "url": "/DSTS2019/img/변성윤.e6259d7b.png"
+  },
+  {
+    "revision": "0a58fdaabef655378485ee41619d5412",
+    "url": "/DSTS2019/img/한성민.0a58fdaa.png"
+  },
+  {
+    "revision": "936984035eccedc94f9ca22c81cd8978",
+    "url": "/DSTS2019/img/scatter-white.93698403.png"
+  },
+  {
+    "revision": "5b1179f720974529f319da67403a5678",
+    "url": "/DSTS2019/img/object-2.5b1179f7.png"
+  },
+  {
+    "revision": "b3b144507afce992c079fbba7fd36609",
+    "url": "/DSTS2019/img/심상훈.b3b14450.jpg"
+  },
+  {
+    "revision": "4250612526c0fe7636b8e46d8c2cf1fb",
+    "url": "/DSTS2019/img/강민구.42506125.png"
+  },
+  {
+    "revision": "7f114801cceeafcfd95b1212094cf4d9",
+    "url": "/DSTS2019/img/이지혜.7f114801.png"
+  },
+  {
+    "revision": "53368fa3d0abf8c316115f636185b0c2",
+    "url": "/DSTS2019/img/강원양.53368fa3.jpg"
+  },
+  {
+    "revision": "7c0096e285c4aeabd02b408e448d81c1",
+    "url": "/DSTS2019/img/main-image.7c0096e2.png"
+  },
+  {
+    "revision": "6540740d969bb6476906034343557974",
+    "url": "/DSTS2019/img/lbd-logo_1.6540740d.png"
+  },
+  {
+    "revision": "47cd94967be0852364ae844db7232505",
+    "url": "/DSTS2019/img/dsts-emblem.47cd9496.png"
+  },
+  {
+    "revision": "ad4b57b8676a1fd9901d",
+    "url": "/DSTS2019/css/app.9c623308.css"
+  },
+  {
+    "revision": "cc1a765d2a555a4e6074",
+    "url": "/DSTS2019/css/about.31e9feea.css"
+  }
+];
